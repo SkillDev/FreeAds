e45e42e7de531697a9e19ef7975cc182151da164
skilldev <anton_m@epitech.eu>
2015-07-06 09:59:10 +0000

    Home Controller

---
+++ freeads/app/Http/Controllers/IndexController.php
@@ -0,0 +1,52 @@
+<?php
+/**
+*PHP version 5
+*File doc comment
+*@category Sniffer
+*@package  Sniffer.Test
+*@author   ANTON Maicmelan <maicmelan.anton@epitech.eu>
+*@license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+*@link     http://intra.epitech.eu
+*/
+namespace App\Http\Controllers;
+use Auth;
+use DB;
+use View;
+/**
+*PHP version 5
+*Class doc domment
+*
+*@category Sniffer
+*@package  Sniffer.Test
+*@author   ANTON Maicmelan <maicmelan.anton@epitech.eu>
+*@license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+*@link     http://intra.epitech.eu
+*/
+class IndexController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Welcome Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller renders the "marketing page" for the application and
+    | is configured to only allow guests. Like most of the other sample
+    | controllers, you are free to modify or remove it as you desire.
+    |
+    */
+    /**
+     * Show the application welcome screen to the user.
+     *
+     * @return Response
+     */
+    public function showIndex()
+    {
+        if (Auth::user()) {
+            $donne = DB::select('select * from annonces ORDER BY RAND()');
+            return View::make('accueil', ['donne' => $donne]);
+        } else {
+            return view('index');
+        }
+    }
+
+}
