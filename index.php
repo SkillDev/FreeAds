90575738874e568972647029d1ca2ef9dd9a8fa1
skilldev <anton_m@epitech.eu>
2015-07-06 10:01:27 +0000

    Redirection

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+*PHP version 5
+*File doc comment
+*@category Sniffer
+*@package  Sniffer.Test
+*@author   ANTON Maicmelan <maicmelan.anton@epitech.eu>
+*@license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+*@link     http://intra.epitech.eu
+*/
+header('Location:freeads/public');
+?>
